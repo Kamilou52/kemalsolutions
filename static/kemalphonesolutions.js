52c93e5901e56651cb54cf035944b9ed8c76fe01
Elkemal <104370591+Elkemal@users.noreply.github.com>
2023-07-28 13:20:02 +0200

    Make my webApp responsive

--- static/kemalphonesolutions.js
+++ static/kemalphonesolutions.js
@@ -1,14 +1,3 @@
-// $("input:modele").on('click', function() {
-//             var $box = $(this);
-//             if ($box.is(":checked")) {
-//               var group = "input:modele[name='" + $box.attr("name") + "']";
-//               $(group).prop("checked", false);
-//               $box.prop("checked", true);
-//             } else {
-//               $box.prop("checked", false);
-//             }
-//           });
-
           $(document).ready(function(){
             $('input[name="modele"]').click(function(){ 
                 $('input[name="modele"]').not(this).prop('checked', false);
