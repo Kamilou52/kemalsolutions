095857709825bb92b1a10b2e8c1c8035453026c6
Elkemal <104370591+Elkemal@users.noreply.github.com>
2023-08-11 16:44:08 +0200

    Another changes on base, JS and css

--- static/kemalphonesolutions.js
+++ static/kemalphonesolutions.js
@@ -3,3 +3,17 @@
                 $('input[name="modele"]').not(this).prop('checked', false);
             });
         });
+
+const hamburgerToggler = document.querySelector(".hamburger")
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+    hamburgerToggler.classList.toggle("open")
+
+    const ariaToggler = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    hamburgerToggler.setAttribute("aria-expanded", ariaToggler)
+
+    navLinksContainer.classList.toggle("open")
+
+}
+hamburgerToggler.addEventListener("click", toggleNav)
